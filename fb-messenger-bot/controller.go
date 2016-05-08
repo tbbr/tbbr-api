f873eb637b17b8691cd699ce788702674dfe822b
MaazAli <maaz_ali@outlook.com>
2016-05-08 13:45:33 -0700

    Update the messenger bot

--- fb-messenger-bot/controller.go
+++ fb-messenger-bot/controller.go
@@ -3,6 +3,7 @@ package fbMessengerBot
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"payup/config"
 
@@ -15,7 +16,7 @@ type fbBotPayload struct {
 }
 
 type entry struct {
-	ID        string      `json:"id"`
+	ID        int         `json:"id"`
 	Time      int         `json:"time"`
 	Messaging []messaging `json:"messaging"`
 }
@@ -28,7 +29,7 @@ type messaging struct {
 }
 
 type user struct {
-	ID string `json:"id"`
+	ID int `json:"id"`
 }
 
 type message struct {
@@ -55,26 +56,96 @@ func VerifyToken(c *gin.Context) {
 // the bot can then subsequently respond to such a message
 func ReceiveMessage(c *gin.Context) {
 	var payload fbBotPayload
+	// foundNotifyMsg := false
 
-	if c.BindJSON(&payload) == nil {
+	defer c.Request.Body.Close()
+	body, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		print(err.Error())
+		c.String(500, "Couldn't parse")
+	}
+
+	marshalErr := json.Unmarshal(body, &payload)
+
+	if marshalErr == nil {
+
+		print(payload.Object)
+		print("\n")
+		print(len(payload.Entry))
+		print("\n")
+
 		messagingEvents := payload.Entry[0].Messaging
-
 		for _, event := range messagingEvents {
+			print(event.Sender.ID)
 			if event.Message.Text == "notify" {
-				c.String(200, "OK")
-				print("We got here")
-
+				print("user wants to be notified")
 				var respMsg sendMessagePayload
 				respMsg.Recipient.ID = event.Sender.ID
 				respMsg.Message.Text = "Okay, I will start sending you notifications"
 
 				jsonPayload, err := json.Marshal(&respMsg)
 
-				if err != nil {
-					http.Post(config.FBMessengerBotPostURL, "application/json", bytes.NewReader(jsonPayload))
+				if err == nil {
+					resp, err := http.Post(config.FBMessengerBotPostURL, "application/json", bytes.NewReader(jsonPayload))
+
+					if err == nil {
+						print(resp.StatusCode)
+					} else {
+						print(err.Error())
+					}
+
+				} else {
+					print(err.Error())
 				}
+			} else {
+				print(event.Message.Text)
 			}
 		}
+		c.String(200, payload.Object)
+	} else {
+		print(marshalErr.Error())
+		c.String(428, "Couldn't parse json")
+		return
 	}
-	c.String(428, "Couldn't parse json")
+
+	// if c.BindJSON(&payload) == nil {
+	// 	messagingEvents := payload.Entry[0].Messaging
+	//
+	// 	for _, event := range messagingEvents {
+	// 		if event.Message.Text == "notify" {
+	// 			foundNotifyMsg = true
+	// 			print("We got here")
+	//
+	// 			var respMsg sendMessagePayload
+	// 			respMsg.Recipient.ID = event.Sender.ID
+	// 			respMsg.Message.Text = "Okay, I will start sending you notifications"
+	//
+	// 			jsonPayload, err := json.Marshal(&respMsg)
+	//
+	// 			if err != nil {
+	// 				http.Post(config.FBMessengerBotPostURL, "application/json", bytes.NewReader(jsonPayload))
+	// 			}
+	// 		} else {
+	// 			var respMsg sendMessagePayload
+	// 			respMsg.Recipient.ID = event.Sender.ID
+	// 			respMsg.Message.Text = "You messaged me!"
+	//
+	// 			jsonPayload, err := json.Marshal(&respMsg)
+	//
+	// 			if err != nil {
+	// 				http.Post(config.FBMessengerBotPostURL, "application/json", bytes.NewReader(jsonPayload))
+	// 			}
+	// 		}
+	// 	}
+	// } else {
+	// 	c.String(428, "Couldn't parse json")
+	// 	return
+	// }
+
+	// if foundNotifyMsg == true {
+	// 	c.String(200, "Notified")
+	// } else {
+	// 	c.String(200, "Worked")
+	// }
 }
