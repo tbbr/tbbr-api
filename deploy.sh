d02747cb7c680a8c6eb67dc1ef9cd76307235179
MaazAli <maaz_ali@outlook.com>
2016-05-08 17:47:32 -0700

    remove deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-export GOOS=linux
-export GOARCH=amd64
-go build -v payup
-ssh root@159.203.33.146 service tbbr stop
-scp payup root@159.203.33.146:/home/tbbr/tbbr-server/tbbr
-ssh root@159.203.33.146 service tbbr start
-export GOOS=darwin
