7f1e10030a68c0e109bea391011c509a1f80168d
Maaz Ali <maaz_ali@outlook.com>
2017-08-15 17:39:05 -0400

    update models

--- models/group.go
+++ models/group.go
@@ -5,41 +5,27 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/tbbr/tbbr-api/config"
-
 	"github.com/jinzhu/gorm"
 	"github.com/manyminds/api2go/jsonapi"
-	"github.com/speps/go-hashids"
+	"github.com/tbbr/tbbr-api/hashid"
 )
 
 // Group model that users wil use
 type Group struct {
-	ID           uint          `json:"-"`
-	Name         string        `json:"name"`
-	Description  string        `json:"description"`
-	Users        []User        `json:"-" gorm:"many2many:group_users;"`
-	Transactions []Transaction `json:"-"`
-	HashID       string        `json:"hashId"`
-	CreatedAt    time.Time     `json:"createdAt"`
-	UpdatedAt    time.Time     `json:"updatedAt"`
-	UserIDs      []uint        `json:"-" sql:"-"`
+	ID          uint      `json:"-"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	HashID      string    `json:"hashId"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+
+	GroupMemberIDs []uint        `json:"-" sql:"-"`
+	GroupMembers   []GroupMember `json:"-" sql:"-"`
 }
 
 // AfterCreate generates a HashID for a Group based on it's numeric ID field
 func (g *Group) AfterCreate(db *gorm.DB) (err error) {
-	hd := hashids.NewData()
-	hd.Salt = config.HashID.Salt
-	hd.MinLength = config.HashID.MinLength
-	h := hashids.NewWithData(hd)
-
-	a := []int{0}
-	a[0] = int(g.ID)
-
-	// Encode
-	e, _ := h.Encode(a)
-	g.HashID = e
-
-	// Save
+	g.HashID = hashid.Generate(g.ID)
 	db.Save(&g)
 	return
 }
@@ -63,15 +49,15 @@ func (g *Group) SetID(id string) error {
 	return nil
 }
 
-// SetToManyReferenceIDs sets the users reference IDs and satisfies the jsonapi.UnmarshalToManyRelations interface
+// SetToManyReferenceIDs sets the groupMembers reference IDs and satisfies the jsonapi.UnmarshalToManyRelations interface
 func (g *Group) SetToManyReferenceIDs(name string, IDs []string) error {
-	if name == "users" {
+	if name == "group-members" {
 		for _, i := range IDs {
 			j, err := strconv.ParseUint(i, 10, 64)
 			if err != nil {
 				return err
 			}
-			g.UserIDs = append(g.UserIDs, uint(j))
+			g.GroupMemberIDs = append(g.GroupMemberIDs, uint(j))
 		}
 	}
 
@@ -82,12 +68,8 @@ func (g *Group) SetToManyReferenceIDs(name string, IDs []string) error {
 func (g Group) GetReferences() []jsonapi.Reference {
 	return []jsonapi.Reference{
 		{
-			Type: "users",
-			Name: "users",
-		},
-		{
-			Type: "transactions",
-			Name: "transactions",
+			Type: "group-members",
+			Name: "groupMembers",
 		},
 	}
 }
@@ -95,19 +77,11 @@ func (g Group) GetReferences() []jsonapi.Reference {
 // GetReferencedIDs satisfies the jsonapi.MarshalLinkedRelations interface
 func (g Group) GetReferencedIDs() []jsonapi.ReferenceID {
 	result := []jsonapi.ReferenceID{}
-	for _, user := range g.Users {
+	for _, member := range g.GroupMembers {
 		result = append(result, jsonapi.ReferenceID{
-			ID:   user.GetID(),
-			Type: "users",
-			Name: "users",
-		})
-	}
-
-	for _, transaction := range g.Transactions {
-		result = append(result, jsonapi.ReferenceID{
-			ID:   transaction.GetID(),
-			Type: "transactions",
-			Name: "transactions",
+			ID:   member.GetID(),
+			Type: "group-members",
+			Name: "groupMembers",
 		})
 	}
 	return result
@@ -116,8 +90,8 @@ func (g Group) GetReferencedIDs() []jsonapi.ReferenceID {
 // GetReferencedStructs to satisfy the jsonapi.MarhsalIncludedRelations interface
 func (g Group) GetReferencedStructs() []jsonapi.MarshalIdentifier {
 	result := []jsonapi.MarshalIdentifier{}
-	for key := range g.Users {
-		result = append(result, g.Users[key])
+	for key := range g.GroupMembers {
+		result = append(result, g.GroupMembers[key])
 	}
 
 	return result
