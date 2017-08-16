a280b2eb4cea6fe373fed7598ea6322721aded05
Maaz Ali <maaz_ali@outlook.com>
2017-08-16 18:49:11 -0400

    Update group transactions table

--- migrations/0015_create_group_transactions_table.up.sql
+++ migrations/0015_create_group_transactions_table.up.sql
@@ -1,18 +1,21 @@
 CREATE TABLE group_transactions (
     id integer NOT NULL,
-    amount integer,
-    senders integer ARRAY NOT NULL,
-    recipients integer ARRAY NOT NULL,
+    amount integer NOT NULL,
+    memo text,
+    sender_ids integer ARRAY NOT NULL,
+    recipient_ids integer ARRAY NOT NULL,
     sender_splits integer ARRAY NOT NULL,
     recipient_splits integer ARRAY NOT NULL,
     sender_split_type text NOT NULL,
     recipient_split_type text NOT NULL,
     group_id integer NOT NULL,
+    creator_id integer NOT NULL,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
     deleted_at timestamp with time zone,
 
-    FOREIGN KEY (group_id) REFERENCES groups(id)
+    FOREIGN KEY (group_id) REFERENCES groups(id),
+    FOREIGN KEY (creator_id) REFERENCES users(id)
 );
 
 CREATE SEQUENCE group_transactions_id_seq
